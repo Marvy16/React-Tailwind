a9f5512a1c262e500139858224c7d94d445fe9c1
mrmarvelworld <ikpmarvel@gmail.com>
2024-11-29 14:23:04 +0100

    added  react files

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+function Hero() {
+  return <div></div>;
+}
+
+export default Hero;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+
+import PopularProducts from "./PopularProduct";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  Subscribe,
+  SuperQuality,
+  SpecialOffer,
+  CustomerReviews,
+  Footer,
+};
